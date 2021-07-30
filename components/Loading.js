11a4fdf79604589a178c08b19756ed88430e339e
Zues45 <85432799+Zues45@users.noreply.github.com>
2021-07-30 13:20:02 +0530

    Update Loading.js

--- components/Loading.js
+++ components/Loading.js
@@ -4,7 +4,7 @@ function Loading() {
     return (
         <centre style={{display: "grid", placeItems: "center", height: "100vh"}}>
             <div>
-                <img src="https://www.freepnglogos.com/uploads/whatsapp-logo-app-png-4.png" 
+                < next img src="https://www.freepnglogos.com/uploads/whatsapp-logo-app-png-4.png" 
                 height={200} 
                 style={{marginBottom: 10}}
                 />
